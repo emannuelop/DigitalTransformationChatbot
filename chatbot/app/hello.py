fb9a8a31dff30ba4b0d17735533a781e3b9b4157
Emannuel Oliveira Pedreira <emannuelop22@gmail.com>
2025-08-20 13:14:34 -0300

    atividade

--- chatbot/app/hello.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Chatbot de Transformação Digital - TCC")
-name = st.text_input("Digite seu nome:")
-if name:
-    st.write(f"Olá, {name}! Bem-vindo ao protótipo do chatbot 🚀")
